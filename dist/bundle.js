5e324327564bdbefd544f474db4544a59c9a132c
Daniel Saputra <danielwetan.io@gmail.com>
2023-05-29 21:46:01 +0700

    project pertama

---
+++ dist/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("function showAlert() {\n  alert(\"Button clicked\");\n}\n\n// Attach the showAlert function to the global scope\nwindow.showAlert = showAlert;\n\nfunction displayName() {\n  alert(\"Muhammad Adam\");\n}\n\nfunction sum(a, b) {\n  return a + b;\n}\n\n\n//# sourceURL=webpack://bootcamp/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+function showAlert() {
+  alert("Button clicked");
+}
+
+// Attach the showAlert function to the global scope
+window.showAlert = showAlert;
+
+function displayName() {
+  alert("Muhammad Adam");
+}
+
+function sum(a, b) {
+  return a + b;
+}
